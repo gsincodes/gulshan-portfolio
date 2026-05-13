d9f2a1c8d466c33ad79117b99747ffc9e6d396d9
gsincodes <gsincodes@gmail.com>
2026-05-14 01:00:34 +0530

    fix introduction.tsx

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -1,46 +1,49 @@
 export default function Introduction() {
     return (
-        <div className="flex min-h-screen flex-col bg-primary font-inconsolata sm:px-2 md:px-4 lg:px-8 xl:px-16 py-4">
+        <div className="h-screen bg-primary font-inconsolata px-4 py-4 grid grid-rows-[1fr_12fr] grid-cols-1">
 
-            <nav className="flex items-center justify-around w-full mb-4">
-                <div className="flex justify-center items-center bg-lime-400 rounded-[200px] w-auto h-15 sm:h-25"> {/* Green rectangle */}
-                <header>
-                    <h1 className="text-[1rem] sm:text-[2rem] font-bold text-black">gulshan(gsincodes){}</h1>
-                </header>
+            <nav className="grid grid-cols-5 items-center row-span-1 p-1">
+                <div className="flex justify-center items-center bg-lime-400 rounded-[200px] w-auto h-full col-span-2 shadow-xl">
+                    <header>
+                        <h1 className="text-[clamp(0.7rem,3vw,4rem)] 2xl:text-[4rem] font-bold text-black">gulshan(gsincodes){}</h1>
+                    </header>
                 </div>
-                <a href="#projects" className="hover:text-gray-900 text-sm font-medium text-gray-600 sm:text-lg lg:text-2xl">Projects</a>
-                <a href="#resume" className="hover:text-gray-900 text-sm font-medium text-gray-600 sm:text-lg lg:text-2xl">Resume</a>
-                <a href="#about" className="hover:text-gray-900 text-sm font-medium text-gray-600 sm:text-lg lg:text-2xl">About</a>
-                <a href="#contact" className="hover:text-gray-900 text-sm font-medium text-gray-600 sm:text-lg lg:text-2xl">Contact</a>
+                <div className="flex col-span-3 justify-evenly h-full items-center">
+                    <a href="#projects" className="hover:text-gray-900 font-medium text-gray-600 text-[clamp(0.7rem,3vw,3rem)] lg:text-2xl">Projects</a>
+                    <a href="#resume" className="hover:text-gray-900 font-medium text-gray-600 text-[clamp(0.7rem,3vw,3rem)] lg:text-2xl">Resume</a>
+                    <a href="#about" className="hover:text-gray-900 font-medium text-gray-600 text-[clamp(0.7rem,3vw,3rem)] lg:text-2xl">About</a>
+                    <a href="#contact" className="hover:text-gray-900 font-medium text-gray-600 text-[clamp(0.7rem,3vw,3rem)] lg:text-2xl">Contact</a>
+                </div>
             </nav>
 
-            {/* <div className="w-[1440px] h-0.5 outline outline-1 outline-offset-[-0.50px] outline-black mt-4"></div> */}
-            <main className="flex flex-col flex-1 justify-evenly text-center">
+            <main className="grid grid-cols-1 grid-rows-[1fr_2fr_1fr]">
 
-                {/*First Gulshan */}
-                <div className="flex md:flex-row flex-col text-[6rem] md:text-[6rem] lg:text-[7rem] xl:text-[12rem] font-bold justify-center items-center text-center leading-none">
+                
+                <div className="flex md:flex-row flex-col text-[clamp(4rem,9vw,10rem)] font-bold justify-center items-center text-center leading-none">
                 <div className="text-white/15 text-stroke-1a">Gulshan</div>
                 <div className="text-black/15">Singh</div>
                 </div>
 
-                <div className="flex flex-col gap-10 items-center text-center leading-none">
-                    <h2 className="flex text-intro text-[3rem] md:text-[4rem] lg:text-[5rem] xl:text-[6rem] font-bold justify-center items-center relative z-10">Hi, I'm</h2>
-                    {/*Second Gulshan */}
-                    <div className="w-full md:w-[633px] lg:w-[738px] xl:w-[1234px] sm:h-full md:h-24 bg-lime-300 md:rounded-[200px] flex items-center justify-center"> {/* Yellow rectangle */}
-                        <div className="flex flex-col text-[6rem] md:text-[6rem] lg:text-[7rem] xl:text-[12rem] md:flex-row font-bold justify-center items-center text-center mt-4">
-                            <h1 className="text-stroke-1b z-10">Gulshan</h1>
-                            <h1 className="text-black z-10">Singh</h1>
+                <div className="flex flex-col justify-evenly items-center text-center leading-none">
+                    <h2 className="flex text-intro text-[clamp(3rem,4vw,6rem)] font-bold justify-center items-center">Hi, I'm</h2>
+                    
+                    
+                    <div className="w-auto bg-lime-300 rounded-[200px] flex items-center justify-center p-4 shadow-xl">
+                        <div className="flex flex-col text-[clamp(4rem,9vw,10rem)] md:flex-row font-bold justify-center items-center text-center mt-4">
+                            <h1 className="text-stroke-1b">Gulshan</h1>
+                            <h1 className="text-black">Singh</h1>
                         </div>
                     </div>
-                    <div> {/*Fullstack Developer*/}
-                        <h2 className="text-black font-bold text-[2rem] md:text-[3rem] xl:text-[6rem] text-center relative z-10">a fullstack developer</h2>
+
+                    <div>
+                        <h2 className="text-black font-bold text-[clamp(2rem,3vw,5rem)] text-center relative z-10">a fullstack developer</h2>
                     </div>
                 </div>
 
-                {/*Third Gulshan */}
-                <div className="flex md:flex-row flex-col text-[6rem] md:text-[6rem] lg:text-[7rem] xl:text-[12rem] font-bold justify-center items-center text-center leading-none">
-                <h1 className="text-white/15 text-stroke-1a">Gulshan</h1>
-                <h1 className="text-black/15">Singh</h1>
+                
+                <div className="flex md:flex-row flex-col text-[clamp(4rem,9vw,10rem)] font-bold justify-center items-center text-center leading-none">
+                    <h1 className="text-white/15 text-stroke-1a">Gulshan</h1>
+                    <h1 className="text-black/15">Singh</h1>
                 </div>
             </main>
         </div>
