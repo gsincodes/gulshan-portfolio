2099c090c786940af1ceba25c0d3a0a6d9fd2c36
gsincodes <gsincodes@gmail.com>
2026-05-18 02:48:04 +0530

    add icon to send button

--- components/SendMeAMessage.tsx
+++ components/SendMeAMessage.tsx
@@ -1,6 +1,7 @@
-import {motion} from "motion/react";
+import {easeOut, motion, scale} from "motion/react";
 import emailjs from "@emailjs/browser";
 import { FormEvent, useState } from "react";
+import Image from "next/image"
 
 export default function SendMeAMessage() {
 
@@ -27,10 +28,10 @@ export default function SendMeAMessage() {
 
     return(
         <div id="contact" className="grid grid-cols-1 grid-rows-[1fr_9fr_1fr] h-screen bg-primary font-inconsolata px-4 py-4">
-            <div className="bg-coolgreen text-black text-[clamp(1.5rem,3vw,5rem)] w-fit p-1 flex items-center font-extrabold shadow-xl">
+            <motion.div initial={{opacity: 0, y: 40}} whileInView={{opacity: 1, y: 0}} transition={{duration: 0.5, ease: easeOut}} className="bg-coolgreen text-black text-[clamp(1.5rem,3vw,5rem)] w-fit p-1 flex items-center font-extrabold shadow-xl">
                 Send Me a Message
-            </div>
-            <div className="flex justify-center items-center">
+            </motion.div>
+            <motion.div initial={{opacity: 0, y: 40}} whileInView={{opacity: 1, y: 0}} transition={{duration: 0.5, ease: easeOut, delay: 0.25}} className="flex justify-center items-center">
                 <form onSubmit={handleSubmit} className="border border-black max-sm:h-[clamp(70px,50vh,700px)] max-md:h-[clamp(70px,55vh,700px)] h-[clamp(70px,70vh,700px)] w-[clamp(100px,90vw,500px)] bg-form grid grid-rows-[1fr_1fr_1fr_1fr] grid-cols-1 p-4 shadow-xl leading-none gap-2">
                     <div className="grid grid-cols-[1fr_2fr] max-md:flex max-md:flex-col h-full col-span-2 gap-y-2">
                         <label htmlFor="name" className="text-[clamp(2rem,5vw,3rem)] md:self-center">Name:</label>
@@ -46,23 +47,26 @@ export default function SendMeAMessage() {
                         <label htmlFor="message" className="text-[clamp(2rem,5vw,3rem)] self-center">Message:</label>
                         <textarea required name="message" value={formData.message} onChange={(e) => setFormData(prev => ({ ...prev, [e.target.name]: e.target.value }))} id="message" className="bg-white text-[clamp(2rem,5vw,3rem)] text-black border border-black h-[clamp(3rem,30vw,10rem)] w-full self-center" />
                     </div>
-                    <motion.button whileTap={{scale: 0.9, y: 3}} type="submit" className="bg-coolgreen rounded-full w-[clamp(2rem,20vw,10rem)] text-black h-[clamp(2rem,5vw,4rem)] justify-self-center self-center shadow-xl border border-black col-span-2">Send</motion.button>
+                    <motion.button whileTap={{scale: 0.9, y: 3}} whileHover={{scale: 1.05}} type="submit" className="bg-coolgreen rounded-full w-[clamp(2rem,20vw,10rem)] text-black h-[clamp(2rem,5vw,4rem)] justify-self-center self-center shadow-xl border border-black col-span-2 cursor-pointer flex items-center justify-center gap-4">
+                        Send 
+                        <Image alt="send-icon" src="/images/send_4077212.png" width={50} height={50} className="w-[30px] h-[30px]" />
+                    </motion.button>
                 </form>
-            </div>
-            <div className="bg-form max-md:flex max-md:flex-col max-md:text-center grid grid-rows-2 grid-cols-1 text-[clamp(1rem,1.5vw,5rem)] shadow-xl text-black">
+            </motion.div>
+            <motion.div initial={{opacity: 0, y: 40}} whileInView={{opacity: 1, y: 0}} transition={{duration: 0.5, ease: easeOut, delay: 0.25}} className="bg-form max-md:flex max-md:flex-col max-md:text-center grid grid-rows-2 grid-cols-1 text-[clamp(1rem,1.5vw,5rem)] shadow-xl text-black">
                 <div className="row-start-1 flex justify-evenly max-md:flex-col">
                     <div>
                         Email: gsincodes@gmail.com
                     </div>
                     <div className="">
                         Check Out My Other Projects:
-                        <a href="">Github</a>
+                        <motion.a whileHover={{scale: 1.5}} href="https://github.com/gsincodes">Github</motion.a>
                     </div>
                 </div>
                 <div className="row-start-2 flex justify-center">
                     gsincodes@2026
                 </div>
-            </div>
+            </motion.div>
         </div>
     );
 }
